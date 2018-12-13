0f66d2096dc2d0aabb47446723a6f8ec87642f86
LucaVazz <luca.vazzano@gmx.net>
2018-12-13 12:40:35 +0100

    remove namespaces

--- cli/things-to-do.ts
+++ cli/things-to-do.ts
@@ -1,12 +1,9 @@
-/// <reference path="../bin/convert.d.ts" />
-
 function showTempConversion() {
-    console.log(`The boiling point of water in F is ${Convert.cToF(100)}`);
+    console.log(`The boiling point of water in F is ${cToF(100)}`);
 }
 
 function showLengthConversion() {
     var i = 35;
     var i = 10;
-    console.log(`${i} inches formats as ${Convert.formatInches(i)}"`);
+    console.log(`${i} inches formats as ${formatInches(i)}"`);
 }
-
