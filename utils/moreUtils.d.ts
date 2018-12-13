fafb240365868a5b6edf4bff2e54d00586aecfa0
LucaVazz <luca.vazzano@gmx.net>
2018-12-13 12:48:30 +0100

    remove outFile

---
+++ utils/moreUtils.d.ts
@@ -0,0 +1,2 @@
+declare function swap(arr: any[], i1: number, i2: number): void;
+//# sourceMappingURL=moreUtils.d.ts.map
---
+++ utils/utils.d.ts
@@ -0,0 +1,3 @@
+declare function leftPad(s: string, n: number): string;
+declare function roundTo(n: number, places: number): number;
+//# sourceMappingURL=utils.d.ts.map
